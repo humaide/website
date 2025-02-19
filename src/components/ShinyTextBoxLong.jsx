6589e3c4fa0a7d755c77113f95ae799efaa960c0
Michelle Wrage <michelle.wrage@student.uni-luebeck.de>
2025-02-19 16:14:18 +0100

    edited font size

--- src/components/ShinyTextBoxLong.jsx
+++ src/components/ShinyTextBoxLong.jsx
@@ -11,7 +11,7 @@ function ShinyTextBoxLong({ title, description, position }) {
                <img src={BoxImgLong} alt="box element" className="flex lg:hidden absolute h-36 min-[425px]:h-48 md:h-52 lg:h-52 xl:h-52 2xl:h-80"/> 
                <div className={`flex items-center justify-center absolute h-24 sm:h-44 max-[425px]:w-full max-lg:w-3/4 lg:w-[36rem] xl:w-[40rem] 2xl:w-[44rem] text-center ${position == "right" ? "text-center lg:text-right" : "text-center lg:text-left"}`}>
                     <div className={`w-full min-[500px]:w-4/5 sm:w-3/4 lg:w-full max-[425px]:pt-11 min-[425px]:pt-16 lg:pt-0 ${position == "right" ? "lg:pr-24" : "lg:pl-24"}`}>
-                        <p className='max-sm:text-sm min-[425px]:text-base lg:text-lg xl:text-xl 2xl:text-4xl px-2 min-[380px]:px-6 sm:px-16 2xl:px-0 text-wrap'><span className='font-semibold max-sm:text-sm min-[425px]:text-base lg:text-lg xl:text-xl 2xl:text-4xl text-wrap'>{title} </span>{description}</p> 
+                        <p className='max-sm:text-xs min-[425px]:text-base lg:text-lg xl:text-xl 2xl:text-4xl px-2 min-[380px]:px-6 sm:px-16 2xl:px-0 text-wrap'><span className='font-semibold max-sm:text-xs min-[425px]:text-base lg:text-lg xl:text-xl 2xl:text-4xl text-wrap'>{title} </span>{description}</p> 
                     </div>
                 </div>
             </motion.div>
