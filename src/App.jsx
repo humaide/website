129842c66d7f70d9a6978441309b100c9e8e22cb
Michelle Wrage <michelle.wrage@student.uni-luebeck.de>
2025-02-21 15:14:06 +0100

    added redirect page

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   return (
     <>
         
-      <BrowserRouter basename='/'>
+      <BrowserRouter>
         <NavBar />
         <Routes>
             <Route path="/" element={<Home />} />
