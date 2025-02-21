0d7fd666e4c8171e67c3192e1623b6d6f91aaac7
Michelle Wrage <michelle.wrage@student.uni-luebeck.de>
2025-02-21 16:00:28 +0100

    edited redirect

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+if (!window.location.hash) {
+  const path = window.location.pathname;
+  window.location.replace(`${window.location.origin}/#${path}`);
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
