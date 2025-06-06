8cb2b999d26fce0e0d0edd898739cee3462dabcc
Michelle Wrage <michelle.wrage@student.uni-luebeck.de>
2025-06-06 03:12:52 +0200

    upated section seperator

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -11,7 +11,7 @@ function AboutSection() {
           {/* Heading */}
           <Heading postion={"text-left pt-10 lg:pt-0"} color={"text-white"} section={t("about.section")} title={t("about.title")} subtitle={t("about.subtitle")} />
           {/* Content Cards */}
-          <div className="flex flex-col md:flex-row items-center justify-center hyphens-auto w-full my-20"> 
+          <div className="flex flex-col md:flex-row items-center justify-center hyphens-auto w-full py-20"> 
             <AboutCard illustration="hc" title={t("about.box-title-hcai")} content={t("about.box-content-hcai")} />
             <AboutCard illustration="act" title={t("about.box-title-aiact")} content={t("about.box-content-aiact")} />
             <AboutCard illustration="sc" title={t("about.box-title-methods")} content={t("about.box-content-methods")} />
