cc3cbdeb9604e6a4fa48071bed6f183a227827b0
michellewrg <michelle.wrage@student.uni-luebeck.de>
2025-09-26 12:36:56 +0200

    added partners banner

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,35 +1,17 @@
-import Heading from "./Heading"
+import Heading from "./Heading";
+import Marquee from "react-fast-marquee";
 
 function Testimonials() {
   return (
-    <div className="bg-white w-full pt-20">
-        <Heading postion={"text-center mb-10 px-16"} color={"text-charcoal"} section={"Network"} title={"Our Partners"} subtitle={"Shaping the future of human-centered AI — Together!"} />
-        <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_400px,_black_calc(100%-400px),transparent_100%)]">
-            <ul className="w-full flex items-center justify-between [&_li]:mx-12 [&_img]:max-w-none infinite-scroll">
-                <li>
-                    <img src="/partner/bipro-logo.svg" alt="BiPro Logo" className="h-10 mx-24" />
-                </li>
-                <li>
-                    <img src="/partner/GW49-logo-blue.svg" alt="Gateway49 Logo" className="h-28 mx-24"/>
-                </li>
-                <li>
-                    <img src="/partner/uzl-logo.png" alt="Universität zu Lübeck Logo" className="h-28 mx-24"/>
-                </li>
-            </ul>
-            <ul className="w-full flex items-center justify-between [&_li]:mx-12 [&_img]:max-w-none infinite-scroll" aria-hidden="true">
-                <li>
-                    <img src="/partner/bipro-logo.svg" alt="BiPro Logo" className="h-10 mx-24" />
-                </li>
-                <li>
-                    <img src="/partner/GW49-logo-blue.svg" alt="Gateway49 Logo" className="h-28 mx-24"/>
-                </li>
-                <li>
-                    <img src="/partner/uzl-logo.png" alt="Universität zu Lübeck Logo" className="h-28 mx-24"/>
-                </li>
-            </ul>
-        </div>
+    <div className="flex flex-col md:flex-row items-center justify-center bg-white w-full pt-28 pb-10 lg:pt-44">
+        <Heading postion={"text-right px-16"} color={"text-charcoal"} section={"Network"} title={"Our Partners"} subtitle={"Shaping the future of human-centered AI — Together!"} />
+        <Marquee gradient={true} className="max-md:pt-10">
+            <img src="/partner/MPGruppe-logo.png" alt="Martens & Prahl Logo" className="h-8 sm:h-10 md:h-10 mx-8 sm:mx-20 md:mx-20" />
+            <img src="/partner/GW49-logo-blue.svg" alt="Gateway49 Logo" className="h-10 sm:h-20 md:h-28 mx-8 sm:mx-20 md:mx-20" />
+            <img src="/partner/UKSH-logo.svg" alt="Universitätsklinikum Schleswig Holstein Logo" className="h-10 sm:h-20 md:h-28 mx-8 sm:mx-20 md:mx-20" />
+        </Marquee>
     </div>
   );
 }
 
-export default Testimonials
+export default Testimonials;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import VideoSection from '../components/VideoSection'
 import TeamSection from '../components/TeamSection'
 import VisionSection from '../components/VisionSection'
 import MissionSection from '../components/MissionSection'
+import Testimonials from '../components/Testimonials'
 import { ThemeConfig } from "flowbite-react";
 import { useTranslation } from "react-i18next";
 
@@ -23,9 +24,9 @@ function Home() {
       <SolutionsSection /> 
       {/* Wave */}
       <div className='w-full h-20 lg:h-1/3 absolute bg-[url(/bg/wave.svg)] bg-cover self-end justify-self-end rotate-180 z-90'></div>             
+      <Testimonials />
       <VideoSection />     
-      {/* <ReportSection /> */}
-      {/* <Testimonials />       */}
+      {/* <ReportSection /> */}   
       <TeamSection />
       <ContactSection section={t("contact.section")} title={t("contact.title")} subtitle={t("contact.subtitle")} />
     </div>
